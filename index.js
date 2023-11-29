6f27f003a4c908e5d153fb598e53d55a72ebaaa5
seasons986 <annalin12345@gmail.com>
2023-11-29 14:13:55 -0500

    flipped x y again

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ window.addEventListener("load", () => {
   function drawCard() {
     if (card > 11) return clearInterval(int);
     var radius = 500
-    var center_x = 400
-    var center_y = 400
+    var center_x = 500
+    var center_y = 350
 
     // ensure that p(r) ~ r instead of p(r) ~ constant
     var r = radius*Math.sqrt(Math.random(1))
@@ -39,10 +39,11 @@ window.addEventListener("load", () => {
     var y = center_y + r*Math.sin(card*30);
     console.log({card,x,y});
 
-    const text = `<!DOCTYPE html><html> <head> <title>${card}</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"><style>body{width:100vw;height:100vh;background-color:${colors[card]};}</style><h1>${card}</h1></body></html>`;
+    const text = `<!DOCTYPE html><html> <head> <title>${card}</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"><style>body{color: #f9faffde;
+      text-shadow: #858ebc -2px 2px 8px;width:100vw;height:100vh;background-color:${colors[card]};}</style><h1>${card+1}</h1></body></html>`;
     const blob = new Blob([text], {type: "text/html"});
     const blobUrl = URL.createObjectURL(blob);
-    window.open(blobUrl, '_blank', `popup,location,status,scrollbars,resizable,width=100,height=100,top=${x},left=${y}`);
+    window.open(blobUrl, '_blank', `popup,location,status,scrollbars,resizable,width=100,height=100,top=${y},left=${x}`);
     window.URL.revokeObjectURL(blobUrl);
     card++;
   }
