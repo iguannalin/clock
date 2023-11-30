140db9a167503f555c3ec60461f21ee8733ebc85
seasons986 <annalin12345@gmail.com>
2023-11-30 00:43:38 -0500

    math

--- index.js
+++ index.js
@@ -24,22 +24,17 @@ window.addEventListener("load", () => {
       "#0d0887"
   ];
 
+  let angle = 0;
+  let step = (2*Math.PI) / 12;
   function drawCard() {
     if (card > 11) return clearInterval(int);
-    var radius = 500
-    var center_x = 500
-    var center_y = 350
-
-    // ensure that p(r) ~ r instead of p(r) ~ constant
-    var r = radius*Math.sqrt(Math.random(1))
-    // var angle = Math.sqrt(2*Math.PI)
-
-    // compute desired coordinates
-    var x = center_x + r*Math.cos(card*30);
-    var y = center_y + r*Math.sin(card*30);
+    let radius = 300;
+    let x = 500 + radius * Math.cos(angle);
+    let y = 375 + radius * Math.sin(angle);
+    angle += step;
     console.log({card,x,y});
 
-    const text = `<!DOCTYPE html><html> <head> <title>${card}</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"><style>body{margin:0 auto;text-align:center;color: #f9faffde;
+    const text = `<!DOCTYPE html><html> <head> <title>${card+1}</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"><style>body{margin:0 auto;text-align:center;color: #f9faffde;
       text-shadow: #858ebc -2px 2px 8px;width:100vw;height:100vh;background-color:${colors[card]};}</style><h1>${card+1}</h1></body></html>`;
     const blob = new Blob([text], {type: "text/html"});
     const blobUrl = URL.createObjectURL(blob);
